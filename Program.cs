38426298a2df1756121db96ac1efa681c9d93d76
fedesiak <120318227+fedesiak@users.noreply.github.com>
2026-06-14 21:17:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace szyfr
+{
+
+    public class Szyfrowanie 
+    {
+        public string zaszyfruj(string tekst, int klucz)
+        {
+            string tekstZaszyfrowany = "";
+            string alfabet = "abcdefghijklmnopqrstuvwxyz";
+
+            for(int i =0; i<tekst.Length; i++)
+            {
+                if (tekst[i] == ' ')
+                {
+                    tekstZaszyfrowany += ' ';
+                }
+                else
+                {
+                    for (int j = 0; j < alfabet.Length; j++)
+                    {
+                        if (tekst[i] == alfabet[j])
+                        {
+                            int index = j + klucz;
+                            if (index >= alfabet.Length)
+                            {
+                                index = index - alfabet.Length;
+                            }
+                            else if (index < 0)
+                            {
+                                index = index + alfabet.Length;
+                            }
+                            tekstZaszyfrowany += alfabet[index];
+                        }
+                    }
+                }
+               
+
+            }
+
+
+            return tekstZaszyfrowany;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Szyfrowanie szyfrowanie = new Szyfrowanie();
+            Console.WriteLine("Podaj tekst: ");
+            string tekst = Console.ReadLine();
+            Console.WriteLine("Podaj klucz: ");
+            int klucz = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zaszyfrowany tekst: "+szyfrowanie.zaszyfruj(tekst, klucz));
+
+
+        }
+    }
+
+}
+
---
+++ Test1.cs
@@ -0,0 +1,64 @@
+﻿using szyfr;
+
+namespace Testy
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void DanePodstawowe()
+        {
+            Szyfrowanie szyfr = new Szyfrowanie();
+            string tekstJawny = "abc";
+            int klucz = 3;
+            string oczekiwane = "def";
+            string aktualne = szyfr.zaszyfruj(tekstJawny, klucz);
+            Assert.AreEqual(oczekiwane, aktualne);
+        }
+
+        [TestMethod]
+        public void Zawijanie()
+        {
+            Szyfrowanie szyfr = new Szyfrowanie();
+            string tekstJawny = "xyz";
+            int klucz = 3;
+            string oczekiwane = "abc";
+            string aktualne = szyfr.zaszyfruj(tekstJawny, klucz);
+            Assert.AreEqual(oczekiwane, aktualne);
+        }
+
+        [TestMethod]
+        public void Odszyfrowanie()
+        {
+            Szyfrowanie szyfr = new Szyfrowanie();
+            string tekstJawny = "def";
+            int klucz = -3;
+            string oczekiwane = "abc";
+            string aktualne = szyfr.zaszyfruj(tekstJawny, klucz);
+            Assert.AreEqual(oczekiwane, aktualne);
+        }
+
+        [TestMethod]
+        public void KluczWiekszyNizDlugoscAlfabetu()
+        {
+            Szyfrowanie szyfr = new Szyfrowanie();
+            string tekstJawny = "abc";
+            int klucz = 29;
+            string oczekiwane = "def";
+            string aktualne = szyfr.zaszyfruj(tekstJawny, klucz);
+            Assert.AreEqual(oczekiwane, aktualne);
+        }
+
+        [TestMethod]
+        public void SpacjaWTekscie()
+        {
+            Szyfrowanie szyfr = new Szyfrowanie();
+            string tekstJawny = "ab cd";
+            int klucz = 2;
+            string oczekiwane = "cd ef";
+            string aktualne = szyfr.zaszyfruj(tekstJawny, klucz);
+            Assert.AreEqual(oczekiwane, aktualne);
+        }
+
+    }
+}
